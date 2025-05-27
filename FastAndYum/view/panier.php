a90633a713491f39f0e9fd239d85b37af6889729
Ayoub <ayoubeddyani4@gmail.com>
2025-05-27 01:46:34 +0100

    creation du page panier.php

---
+++ FastAndYum/view/panier.php
@@ -0,0 +1,92 @@
+<?php include 'includes/header.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title></title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+
+
+    <!-- Fast&Yumicon -->
+<link href="/FastAndYumProject/FastAndYum/public/img/logo1.png" rel="icon">
+
+
+    <!-- Icons of the site -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Maktabat Stylesheet -->
+    <link href="/FastAndYumProject/FastAndYum/public/lib/animate/animate.min.css" rel="stylesheet">
+    <link href="/FastAndYumProject/FastAndYum/public/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+    <!-- Bootstrap -->
+    <link href="/FastAndYumProject/FastAndYum/public/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- CSS Stylesheet -->
+    <link href="/FastAndYumProject/FastAndYum/public/css/fastyum.css" rel="stylesheet">
+    <style></style>
+
+</head>
+
+<body class="panierbody">
+<br><br>
+<div class="container my-5">
+    <form action="#" method="post">
+    <div class="mb-3">
+        <a href="index.php?url=menu.php" class="text-decoration-none">&larr; Poursuivre les achats</a>
+    </div>
+
+    <h5 class="text-warning mb-4">Panier</h5>
+
+    <div class="row">
+        <div class="col-md-8">
+            <div class="cart-item">
+                <img src="/FastAndYumProject/FastAndYum/public/img/Menu/STAR BACON.jpg" alt="Pizza Italien">
+                <div class="flex-grow-1">
+                    <div><strong>Pizza italien</strong></div>
+                    <small>Extra cheese and toping</small>
+                </div>
+                <input type="number" class="form-control w-25 mx-2" name="qty_pizza" value="2">
+                <div class="me-3">50.00DH</div>
+                <button type="button" class="btn btn-sm btn-outline-danger">🗑</button>
+            </div>
+        </div>
+
+       
+    </div>
+</form>
+
+</div>
+
+
+<br><br><br>
+    
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="/FastAndYumProject/FastAndYum/public/lib/wow/wow.min.js"></script>
+
+    <script src="/FastAndYumProject/FastAndYum/public/lib/owlcarousel/owl.carousel.min.js"></script>
+
+    <!-- Javascript -->
+    <script src="/FastAndYumProject/FastAndYum/public/js/main.js"></script>
+
+
+
+
+
+
+
+
+
+
+
+</body>
+
+</html>
+<?php include 'includes/footer.php'; ?>
